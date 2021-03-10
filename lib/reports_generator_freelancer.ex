afea51cee7801decad7216b1bd060ccd82e7fefe
brenoos <breno.oliver07@gmail.com>
2021-03-10 12:51:32 -0300

    initial commit

---
+++ lib/reports_generator_freelancer.ex
@@ -0,0 +1,18 @@
+defmodule ReportsGeneratorFreelancer do
+  @moduledoc """
+  Documentation for `ReportsGeneratorFreelancer`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ReportsGeneratorFreelancer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/reports_generator_freelancer_test.exs
@@ -0,0 +1,8 @@
+defmodule ReportsGeneratorFreelancerTest do
+  use ExUnit.Case
+  doctest ReportsGeneratorFreelancer
+
+  test "greets the world" do
+    assert ReportsGeneratorFreelancer.hello() == :world
+  end
+end
