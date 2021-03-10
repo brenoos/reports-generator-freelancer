a4157a28d4a6237011c6a7142a7921b805b94813
brenoos <breno.oliver07@gmail.com>
2021-03-10 13:42:39 -0300

    parse file

---
+++ lib/parser.ex
@@ -0,0 +1,21 @@
+defmodule ReportsGeneratorFreelancer.Parser do
+  def parse_file(filename) do
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Stream.map(fn line -> parse_line(line) end)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> Enum.map(fn string -> convert_integer(string) end)
+  end
+
+  defp convert_integer(string) do
+    case Integer.parse(string) do
+      {int, _string} -> int
+      :error -> string
+    end
+  end
+end
